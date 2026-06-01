4142ae8d83abbd1be9cd80f197ddd042b8540215
legnaro72 <massimiliano.ferrando@gmail.com>
2026-06-01 22:40:43 +0200

    manifest

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'ddgff-pwa-v3';
+const CACHE_VERSION = 'ddgff-pwa-v4';
 const CACHE_PREFIX = 'ddgff-pwa-';
 const STATIC_CACHE = `${CACHE_VERSION}-static`;
 const RUNTIME_CACHE = `${CACHE_VERSION}-runtime`;
